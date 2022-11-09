9825fff82eb40acc94c21802c51a2ce27b18d575
kmdshojib <kmd.shojib123@gmail.com>
2022-11-10 01:38:19 +0600

    spinner added

--- src/routes/privateroute.js
+++ src/routes/privateroute.js
@@ -2,10 +2,19 @@ import React, { useContext } from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
 import { AuthContext } from '../context/userContext'
 
+const Spinner = () => (
+    <div class="flex justify-center items-center">
+        <div class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+)
+
 const PrivateRoute = ({children}) => {
     const {user,loading} = useContext(AuthContext)
     const location = useLocation()
     console.log(loading)
+    loading && <Spinner />
     return (!user) ?  <Navigate to="/login" state={{from: location}} replace /> :  children
 }
 
